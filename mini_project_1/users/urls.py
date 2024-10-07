8b5b2cea8edc5a7f0048ad150f6856415b57ea9b
neemaque <nurmukhan.n.b@gmail.com>
2024-10-07 18:30:59 +0500

    Profile changes
    Profile edit, register, follow and unfollow

--- mini_project_1/users/urls.py
+++ mini_project_1/users/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
     path('users/<int:user_id>/', views.profile),
+    path('users/<int:user_id>/edit/', views.edit_profile),
+    path('users/<int:user_id>/follow/', views.follow_user),
+    path('users/<int:user_id>/unfollow/', views.unfollow_user),
+    path('register/', views.register)
 ]
--- mini_project_1/users/views.py
+++ mini_project_1/users/views.py
@@ -3,6 +3,7 @@ from django.template import loader
 from django.http import HttpResponse
 from .models import User, Profile, Follow
 
+
 def profile(request, user_id):
     user = User.objects.all().get(id=user_id)
     all_profiles = Profile.objects.all()
@@ -17,15 +18,20 @@ def profile(request, user_id):
     context = {'profile': profile, 'followers': followers}
     return HttpResponse(template.render(context, request))
 
+
 def edit_profile(request, user_id):
+    user = User.objects.all().get(id=user_id)
+    all_profiles = Profile.objects.all()
+    result = []
+    for a in all_profiles:
+        if a.user.id == user_id:
+            result.append(a)
+    profile = result[0]
+    if request.method == 'GET':
+        template = loader.get_template('profile_edit_template.html')
+        context = {'profile': profile}
+        return HttpResponse(template.render(context, request))
     if request.method == 'POST':
-        user = User.objects.all().get(id=user_id)
-        all_profiles = Profile.objects.all()
-        result = []
-        for a in all_profiles:
-            if a.user.id == user_id:
-                result.append(a)
-        profile = result[0]
         name = request.POST.get('name')
         user.name = name
         user.save()
@@ -35,3 +41,49 @@ def edit_profile(request, user_id):
         return HttpResponse("Success!")
 
 
+def follow_user(request, user_id):
+    user = User.objects.all().get(id=user_id)
+    if request.method == 'POST':
+        follower_id = int(request.POST.get('follower_id'))
+        follower = User.objects.all().get(id=follower_id)
+        result = []
+        for a in Follow.objects.all():
+            if a.Follower == follower and a.Following == user:
+                result.append(a)
+        if len(result) != 0:
+            return HttpResponse("Already follows!")
+        follow = Follow(Follower = follower, Following = user)
+        follow.save()
+        return HttpResponse("Success!")
+
+
+def unfollow_user(request, user_id):
+    user = User.objects.all().get(id=user_id)
+    if request.method == 'POST':
+        follower_id = int(request.POST.get('follower_id'))
+        follower = User.objects.all().get(id=follower_id)
+        result = []
+        for a in Follow.objects.all():
+            if a.Follower == follower and a.Following == user:
+                result.append(a)
+        try:
+            follow = result[0]
+            follow.delete()
+            return HttpResponse("Success!")
+        except:
+            return HttpResponse("User doesn't follow!")
+
+
+def register(request):
+    if request.method == 'GET':
+        template = loader.get_template('register_template.html')
+        context = {}
+        return HttpResponse(template.render(context, request))
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        user = User(name=name)
+        user.save()
+        bio = request.POST.get('bio')
+        profile = Profile(user=user, bio=bio)
+        profile.save()
+        return HttpResponse("Success!")
